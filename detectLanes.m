0125523808e74b7b6d21e9de5f083ba714db3b05
Rishi Bhuta <rbhuta@umich.edu>
2017-03-09 17:22:57 -0500

    updated int_image tophat filter

--- detectLanes.m
+++ detectLanes.m
@@ -3,7 +3,7 @@ function [detected_lanes] = detectLanes(img_path,horizon, width)
 % Load in image
 imFile = fullfile(img_path);
 img = imread(imFile);
-tic
+tic;
 % Convert to grayscale
 img_gray = rgb2gray(img);
 
@@ -15,39 +15,45 @@ padSize = (filterSize-1)/2;
 Apad = padarray(img_gray, padSize, 'replicate','both');
 intA = integralImage(Apad);
 int_img = integralBoxFilter(intA, filterSize);
-img_int = uint8(int_img);
+filt_img = uint8(int_img);
 
 % Tophat filtering 
 slope = -width / (1024 - horizon);
 
 j = 0;
 % Computing width of markers into array
-for i=size(img_int,1):-1:horizon
+for i=size(filt_img,1):-1:horizon
     SM(i) = max(round(width + j*slope),1);
     j = j + 1;
 end
 s = width;
-thresh = 10;
+thresh = 5;
 img_grad = img_gray;
 
-col = size(img_int,2);
-for v = size(img_int,1):-1:horizon
+col = size(filt_img,2);
+for v = size(filt_img,1):-1:horizon
+    % compute int image
+    sum = double(filt_img(v,1));
     for u = 1:col
-        u_max = min(col, u+6*s);
-        u_min = max(col, u-6*s);
+        int_img_gray(v,u) = sum;
+        sum = sum + double(filt_img(v,u));
+    end
+
+    for u = 1:col
+        u_max = min(col, u+6*SM(v));
+        u_min = max(col, u-6*SM(v));
         du = round(u_max-u_min)+1;
-        if (img_gray(v,u) > (thresh+(img_int(v,round(u_max))-img_int(v,round(u_min)))/du))
+        if (img_gray(v,u) > (thresh+(int_img_gray(v,round(u_max))-int_img_gray(v,round(u_min)))/du))
             img_grad(v,u) = 255;
         end
     end
     s = round(SM(v)*0.5)+1;
     for u = 2*s+1:col-2*s
-        top_hat(v,u) = 1/(4*s)*(2*(int_img(v,u+s)-int_img(v,u-s))-(int_img(v,u+2*s)-int_img(v,u-2*s)));
+        top_hat(v,u) = 1/(4*s)*(2*(int_img_gray(v,u+s)-int_img_gray(v,u-s))-(int_img_gray(v,u+2*s)-int_img_gray(v,u-2*s)));
     end
     thresh_tophat = 5;
 end
-toc
-    
+toc;
     
 % Binarize and cut off above horizon
 detected_lanes = (top_hat) > (1.5);
