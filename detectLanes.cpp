b1996710b1dc6cc8384cb9b036497b41377ef338
Rishi Bhuta <rbhuta@umich.edu>
2017-03-31 19:19:04 -0400

    added support for autocycling through all folders & images

--- detectLanes.cpp
+++ detectLanes.cpp
@@ -10,15 +10,14 @@ using namespace cv;
 using namespace std;
 
 /* RUN COMMAND:
-g++ `pkg-config --cflags opencv` detectLanes.cpp `pkg-config --libs opencv` -o test; ./test 
+g++ `pkg-config --cflags opencv` detectLanes.cpp `pkg-config --libs opencv` -O3 -o detect; ./detect 
 
 TODO:
 - put functions into class
 - add gui
 - file explore via boost filesystem
-- eigen speed up
+- optimize speeds
 ------------------------------------------
-- test pre-processing gaussian & mean box filter
 - optimize otsu thresholding
 
 DONE:
@@ -26,6 +25,8 @@ DONE:
 - loc_grad
 - sym_loc_grad
 - otsu?
+- auto cycle through folders + images
+- test pre-processing gaussian & mean box filter
 */
 
 void tophat_filter(vector<double>& Row_Int_Image, int row_idx, Mat& TopHat_Image, double s);
@@ -42,121 +43,146 @@ string combined_window = "Combined Filter";
 
 int main( int, char** argv )
 {
+	// Load all folders
+	vector<string> folders;
+	folders.push_back("roma/BDXD54/");
+	folders.push_back("roma/BDXN01/");
+	folders.push_back("roma/IRC041500/");
+	folders.push_back("roma/IRC04510/");
+	folders.push_back("roma/LRAlargeur13032003/");
+	folders.push_back("roma/LRAlargeur14062002/");
+	folders.push_back("roma/LRAlargeur26032003/");
+	folders.push_back("roma/RD116/");
+	folders.push_back("roma/RouenN8IRC051900/");
+	folders.push_back("roma/RouenN8IRC052310/");
 
-	// Collect image and param paths
-	string image_path = "roma/BDXD54/";
-	string param_path = "roma/BDXD54/";
-	image_path += "IMG00006.jpg";
-	param_path += "param.cal"; 
+	// Set the base folder
+	string base_string;
+	for (int f = 0; f < folders.size(); ++f) {
+		base_string = folders[f];
 
-	// Read in image and horizon 
-	src = imread(image_path, IMREAD_GRAYSCALE); // Load image as grayscale
-	if(src.empty()) {
-		cout << "Unable to read image" << endl;
-		return 0;
-	}
+		// Collect image and param paths
+		string image_folder_path = base_string;
+		string param_path = base_string;
+		string image_path = base_string;
+		image_folder_path += "img.mov";
+		param_path += "param.cal"; 
 
-	// Pre processing filtering
-	blur(src, src, Size(9, 9));
-	//GaussianBlur(src, src, Size(9, 9), 0);
+		ifstream imgs(image_folder_path.c_str());
 
-	int horizon, lane_width;
-	double ignore;
-	ifstream param(param_path.c_str());
-	param >> horizon >> ignore >> lane_width;
+		int num_pics = 0;
+		imgs >> num_pics;
+		vector<string> image_extensions(num_pics);
+		for (int p = 0; p < num_pics; ++p) {
 
-	// Define Acceptability Range
-	double SM_max = lane_width*6; // pixels in 30 cm
-	double Sm_max = lane_width*2; // pixels in 10 cm
-	vector<double> SM(src.rows);
-	vector<double> Sm(src.rows);
-	for (int i = src.rows; i > horizon; --i)
-	{
-		SM[i] = SM_max / (src.rows - horizon) * (i - horizon); 
-		Sm[i] = Sm_max / (src.rows - horizon) * (i - horizon);
-	}
+			image_path = base_string;
+			imgs >> image_extensions[p];
+			image_path += image_extensions[p]; 
 
-	//Define thresholds
-	int thresh_grad = 30;
-	int thresh_sym_grad = 20;
+			// Read in image and horizon 
+			src = imread(image_path, IMREAD_GRAYSCALE); // Load image as grayscale
+			if(src.empty()) {
+				cout << "Unable to read image" << endl;
+				return 0;
+			}
 
-	vector<double> Row_Int_Image(src.cols); // Init vector length
-	Mat TopHat_Image = Mat::zeros(src.rows, src.cols, CV_64FC1); // Init Tophat image matrix
-	Mat Loc_Grad_Image = Mat::zeros(src.rows, src.cols, CV_8UC1); // Init Loc_Gradient image matrix
-	Mat Sym_Loc_Grad_Image = Mat::zeros(src.rows, src.cols, CV_8UC1); // Init Loc_Gradient image matrix
+			// Pre processing filtering
+			//blur(src, src, Size(9, 9));
+			//GaussianBlur(src, src, Size(9, 9), 0);
 
-	for (int i = src.rows - 1; i > horizon; --i) {
-		// Compute Individual Row by Row Integral Image
-		int sum = (int)src.at<uchar>(i, 0);
-		for (int j = 0; j < src.cols; ++j) {
+			int horizon, lane_width;
+			double ignore;
+			ifstream param(param_path.c_str());
+			param >> horizon >> ignore >> lane_width;
 
-			Row_Int_Image[j] = sum;
-			sum += (int)src.at<uchar>(i,j);
+			// Define Acceptability Range
+			double SM_max = lane_width*6; // pixels in 30 cm
+			double Sm_max = lane_width*2; // pixels in 10 cm
+			vector<double> SM(src.rows);
+			vector<double> Sm(src.rows);
+			for (int i = src.rows; i > horizon; --i)
+			{
+				SM[i] = SM_max / (src.rows - horizon) * (i - horizon); 
+				Sm[i] = Sm_max / (src.rows - horizon) * (i - horizon);
+			}
+
+			//Define thresholds
+			int thresh_grad = 30;
+			int thresh_sym_grad = 20;
+
+			vector<double> Row_Int_Image(src.cols); // Init vector length
+			Mat TopHat_Image = Mat::zeros(src.rows, src.cols, CV_64FC1); // Init Tophat image matrix
+			Mat Loc_Grad_Image = Mat::zeros(src.rows, src.cols, CV_8UC1); // Init Loc_Gradient image matrix
+			Mat Sym_Loc_Grad_Image = Mat::zeros(src.rows, src.cols, CV_8UC1); // Init Loc_Gradient image matrix
+
+			for (int i = src.rows - 1; i > horizon; --i) {
+				// Compute Individual Row by Row Integral Image
+				int sum = (int)src.at<uchar>(i, 0);
+				for (int j = 0; j < src.cols; ++j) {
+
+					Row_Int_Image[j] = sum;
+					sum += (int)src.at<uchar>(i,j);
+				}
+
+				// Apply TopHat Filter
+				double s = round((SM[i] + Sm[i])*(0.25));
+				if ((int)s % 2 == 0) { s += 1;} // s is always odd
+
+				tophat_filter(Row_Int_Image, i, TopHat_Image, s);
+
+				local_gradient(Row_Int_Image, i, Loc_Grad_Image, s, thresh_grad);
+				
+				sym_local_gradient(Row_Int_Image, i, Sym_Loc_Grad_Image, s, thresh_sym_grad);
+				
+				
+			}
+
+			//filter by lane marking width
+			lane_width_filter(Loc_Grad_Image, Sm, SM, horizon);
+			lane_width_filter(Sym_Loc_Grad_Image, Sm, SM, horizon);
+
+			// Binarize Tophat
+			Mat tophat_binarized;
+			TopHat_Image.convertTo(TopHat_Image,CV_8UC1, 10);
+			threshold(TopHat_Image, tophat_binarized, 12, 255, THRESH_BINARY + THRESH_OTSU );
+
+			//Combine Images
+			uchar scalar = 255;
+			Mat Combined_Image = (Loc_Grad_Image / scalar).mul(tophat_binarized);
+			Combined_Image = Combined_Image.mul((Sym_Loc_Grad_Image / scalar));
+
+
+			//Display TopHat Results
+			namedWindow(tophat_window, WINDOW_NORMAL); // Create a window to display results
+			resizeWindow(tophat_window, 512,  432);
+			imshow(tophat_window, tophat_binarized);
+			moveWindow(tophat_window, 20, 50);
+			imwrite("tophat.jpg", tophat_binarized);
+
+			//Display Loc Gradient Results 
+			namedWindow(local_grad_window, WINDOW_NORMAL); // Create a window to display results
+			resizeWindow(local_grad_window, 512,  432);
+			imshow(local_grad_window, Loc_Grad_Image);
+			moveWindow(local_grad_window, 680, 50);
+			imwrite("loc_grad.jpg", Loc_Grad_Image);
+
+			//Display Loc Gradient Results 
+			namedWindow(sym_local_grad_window, WINDOW_NORMAL); // Create a window to display results
+			resizeWindow(sym_local_grad_window, 512,  432);
+			imshow(sym_local_grad_window, Sym_Loc_Grad_Image);
+			moveWindow(sym_local_grad_window, 1340, 50);
+			imwrite("sym_loc_grad.jpg", Sym_Loc_Grad_Image);
+
+			//Display Combined Results 
+			namedWindow(combined_window, WINDOW_NORMAL); // Create a window to display results
+			resizeWindow(combined_window, 512,  432);
+			imshow(combined_window, Combined_Image);
+			moveWindow(combined_window, 680, 550);
+			imwrite("combined_filter.jpg", Combined_Image);
+
+			waitKey(0);
 		}
-
-		// Apply TopHat Filter
-		double s = round((SM[i] + Sm[i])*(0.25));
-		if ((int)s % 2 == 0) { s += 1;} // s is always odd
-
-		tophat_filter(Row_Int_Image, i, TopHat_Image, s);
-
-		local_gradient(Row_Int_Image, i, Loc_Grad_Image, s, thresh_grad);
-		
-		sym_local_gradient(Row_Int_Image, i, Sym_Loc_Grad_Image, s, thresh_sym_grad);
-		
-		
 	}
-
-	//filter by lane marking width
-	lane_width_filter(Loc_Grad_Image, Sm, SM, horizon);
-	lane_width_filter(Sym_Loc_Grad_Image, Sm, SM, horizon);
-
-	// Binarize Tophat
-	Mat tophat_binarized;
-	TopHat_Image.convertTo(TopHat_Image,CV_8UC1, 10);
-	threshold(TopHat_Image, tophat_binarized, 12, 255, THRESH_BINARY + THRESH_OTSU );
-
-	//Combine Images
-	// vector<Mat> channels;
-	// channels
-	// div by 255 
-	uchar scalar = 255;
-	Mat Combined_Image = (Loc_Grad_Image / scalar).mul(tophat_binarized);
-	Combined_Image = Combined_Image.mul((Sym_Loc_Grad_Image / scalar));
-
-
-	//Display TopHat Results
-	// Mat tophat_binarized;
-	// TopHat_Image.convertTo(TopHat_Image,CV_8UC1, 10);
-	// threshold(TopHat_Image, tophat_binarized, 12, 255, THRESH_BINARY + THRESH_OTSU );
-	namedWindow(tophat_window, WINDOW_NORMAL); // Create a window to display results
-	resizeWindow(tophat_window, 512,  432);
-	imshow(tophat_window, tophat_binarized);
-	moveWindow(tophat_window, 20, 100);
-	imwrite("tophat.jpg", tophat_binarized);
-
-	//Display Loc Gradient Results 
-	namedWindow(local_grad_window, WINDOW_NORMAL); // Create a window to display results
-	resizeWindow(local_grad_window, 512,  432);
-	imshow(local_grad_window, Loc_Grad_Image);
-	moveWindow(local_grad_window, 680, 100);
-	imwrite("loc_grad.jpg", Loc_Grad_Image);
-
-	//Display Loc Gradient Results 
-	namedWindow(sym_local_grad_window, WINDOW_NORMAL); // Create a window to display results
-	resizeWindow(sym_local_grad_window, 512,  432);
-	imshow(sym_local_grad_window, Sym_Loc_Grad_Image);
-	moveWindow(sym_local_grad_window, 1340, 100);
-	imwrite("sym_loc_grad.jpg", Sym_Loc_Grad_Image);
-
-	//Display Combined Results 
-	namedWindow(combined_window, WINDOW_NORMAL); // Create a window to display results
-	resizeWindow(combined_window, 512,  432);
-	imshow(combined_window, Combined_Image);
-	moveWindow(combined_window, 680, 550);
-	imwrite("sym_loc_grad.jpg", Combined_Image);
-
-	waitKey(0);
 
 	return 0;
 }
